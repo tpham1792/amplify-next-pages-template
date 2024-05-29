14347538fa829e33fd9b55d3e00ed820e4d10eb2
Thi Thu Thao PHAM (TPM) <thithuthao.pham@entech-se.com>
2024-05-29 16:32:17 +0200

    back to no ampx sandbox

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,7 +10,8 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-    }).authorization(allow => [allow.owner()]),
+    })
+    .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -18,13 +19,12 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    // defaultAuthorizationMode: "apiKey",
-    // // API Key is used for a.allow.public() rules
-    // apiKeyAuthorizationMode: {
-    //   expiresInDays: 30,
-    defaultAuthorizationMode: 'userPool',
+    defaultAuthorizationMode: "apiKey",
+    // API Key is used for a.allow.public() rules
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
     },
-  // },
+  },
 });
 
 /*== STEP 2 ===============================================================
