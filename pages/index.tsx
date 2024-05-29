8932625f2b876ae3f3aa39e017a821766eab6ca5
tpham1792 <kunpham2k@gmail.com>
2024-05-29 15:47:27 +0200

    added per-user data isolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,8 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>My todos</h1>
+          {/* <h1>My todos</h1> */}
+          <h1>{user?.signInDetails?.loginId}'s todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
